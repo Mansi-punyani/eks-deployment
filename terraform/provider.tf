568ba3dd560c077dcbef91f9eaa8400052cea665
Mansi-punyani <72453933+Mansi-punyani@users.noreply.github.com>
2024-01-23 15:31:06 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAUJUGYM7MIPPAZUDR"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
